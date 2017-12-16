d63f14ffa036ca994d4df917ed672b76ae9a343b
fallen <haiyan.xu.vip@gmail.com>
2017-12-16 15:07:26 +0800

    *

--- src/main/java/flying/config/MybatisPlusConfig.java
+++ src/main/java/flying/config/MybatisPlusConfig.java
@@ -3,9 +3,6 @@ package flying.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-// https://gitee.com/baomidou/mybatisplus-spring-boot/tree/master
-
-//@Configuration
 @Configuration
 @MapperScan("flying.mapper.sparrow")
 public class MybatisPlusConfig {
