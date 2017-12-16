3fe20f4c125aaf9a9280b3711c9c3398005ed0ac
fallen <haiyan.xu.vip@gmail.com>
2017-12-16 09:37:53 +0800

    *

---
+++ src/main/java/flying/FlyingApplication.java
@@ -0,0 +1,12 @@
+package flying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingApplication.class, args);
+	}
+}
---
+++ src/test/java/flying/FlyingApplicationTests.java
@@ -0,0 +1,16 @@
+package flying;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
