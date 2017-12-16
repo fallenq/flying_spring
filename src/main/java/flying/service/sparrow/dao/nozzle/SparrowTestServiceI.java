194e9936cf3824b7a62002c0acec7b0bf4d57cd9
roger <rogerk08@126.com>
2017-12-16 22:15:55 +0800

    *

--- src/main/java/flying/service/sparrow/dao/nozzle/SparrowTestServiceI.java
+++ src/main/java/flying/service/sparrow/dao/nozzle/SparrowTestServiceI.java
@@ -3,7 +3,6 @@ package flying.service.sparrow.dao.nozzle;
 import com.baomidou.mybatisplus.service.IService;
 
 import flying.entity.sparrow.SparrowTest;
-import flying.mapper.sparrow.SparrowTestMapper;
 
 /**
  * <p>
