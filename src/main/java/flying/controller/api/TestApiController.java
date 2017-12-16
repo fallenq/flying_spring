88b4528c085b4b2c14d470cf98aaac84b22fd3a7
fallen <haiyan.xu.vip@gmail.com>
2017-12-16 15:20:04 +0800

    *

--- src/main/java/flying/controller/api/TestApiController.java
+++ src/main/java/flying/controller/api/TestApiController.java
@@ -11,20 +11,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import flying.entity.sparrow.SparrowTest;
-import flying.service.sparrow.dao.impl.SparrowTestServiceImpl;
+import flying.service.sparrow.dao.nozzle.SparrowTestServiceI;
 import flying.service.sparrow.nozzle.SpTestServiceI;
 
 @RestController
 public class TestApiController {
 	
 	@Autowired
-	private SparrowTestServiceImpl testService;
+	private SparrowTestServiceI testService;
 	
 	@RequestMapping("/")
 	@ResponseBody
 	public Map<String, String> index(HttpServletRequest request) {
-//		SparrowTest test = testService.selectById(1);
-//		System.out.println(test);
+		SparrowTest test = testService.selectById(1);
+		System.out.println(test);
 		Map<String, String> result = new HashMap<String, String>();
 		result.put("test", "hello");
 		return result;
--- src/main/java/flying/service/sparrow/impl/SpTestImpl.java
+++ src/main/java/flying/service/sparrow/impl/SpTestImpl.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Service;
 
 import flying.entity.sparrow.SparrowTest;
 import flying.service.sparrow.dao.impl.SparrowTestServiceImpl;
+import flying.service.sparrow.dao.nozzle.SparrowTestServiceI;
 import flying.service.sparrow.nozzle.SpTestServiceI;
 
 @Service
 public class SpTestImpl implements SpTestServiceI {
 
 	@Autowired
-	private SparrowTestServiceImpl testService;
+	private SparrowTestServiceI testService;
 
 	@Override
 	public SparrowTest getById(int id) {
