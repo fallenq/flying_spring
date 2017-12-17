c17ae8131271dc574f0d30676d982e6b55b67fb9
roger <rogerk08@126.com>
2017-12-17 17:24:59 +0800

    *

--- src/main/java/flying/FlyingApplication.java
+++ src/main/java/flying/FlyingApplication.java
@@ -1,9 +1,11 @@
 package flying;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("flying.mapper.sparrow*")
 public class FlyingApplication {
 
 	public static void main(String[] args) {
--- src/main/java/flying/controller/api/TestApiController.java
+++ src/main/java/flying/controller/api/TestApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import flying.entity.sparrow.SparrowTest;
 import flying.service.sparrow.dao.nozzle.SparrowTestServiceI;
 import flying.service.sparrow.nozzle.SpTestServiceI;
 
@@ -23,10 +24,11 @@ public class TestApiController {
 	@RequestMapping("/")
 	@ResponseBody
 	public Map<String, String> index(HttpServletRequest request) {
+//		SparrowTest test = new SparrowTest();
 //		System.out.println(testService.getById(1));
 		Map<String, String> result = new HashMap<String, String>();
-		HttpSession session = request.getSession();
-		session.setAttribute("test", "world");
+//		HttpSession session = request.getSession();
+//		session.setAttribute("test", "world");
 		result.put("test", "hello");
 		return result;
 	}
