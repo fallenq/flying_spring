c577f698bf9300e3acaf4bc9f441c11ce7adb879
roger <rogerk08@126.com>
2017-12-17 19:33:27 +0800

    *

--- src/main/java/flying/controller/api/CommonApiController.java
+++ src/main/java/flying/controller/api/CommonApiController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
-public class UserApiController {
+@RequestMapping("/api/common")
+public class CommonApiController {
 
 }
