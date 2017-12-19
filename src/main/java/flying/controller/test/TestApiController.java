662ae9c1fb3a260f9206a25387504544a50790a1
fallen <haiyan.xu.vip@gmail.com>
2017-12-19 15:48:07 +0800

    *

--- src/main/java/flying/controller/test/TestApiController.java
+++ src/main/java/flying/controller/test/TestApiController.java
@@ -4,27 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import flying.entity.sparrow.SparrowTest;
-import flying.service.sparrow.basic.SpTestService;
-import flying.service.sparrow.dao.nozzle.SparrowTestServiceI;
 
 @RestController
 @RequestMapping("/api/test")
 public class TestApiController {
 	
-	@Autowired
-	private SpTestService testService;
+//	@Autowired
+//	private SpTestService testService;
 	
 	@RequestMapping("/index")
 	public Map<String, String> index(HttpServletRequest request) {
-		System.out.println(testService.getById(1));
+//		System.out.println(testService.getById(1));
 		Map<String, String> result = new HashMap<String, String>();
 //		HttpSession session = request.getSession();
 //		session.setAttribute("test", "world");
